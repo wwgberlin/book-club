2c86a2b6f424403927099dae240d07a15234ba20
Asalle <asalle.kim@gmail.com>
2020-07-18 15:28:36 +0200

    Add main

--- interpreter_book/src/cmd/interpreter/main.go
+++ interpreter_book/src/cmd/interpreter/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/Asalle/monkey-interpreter/repl"
+	"os"
+	"os/user"
+)
 
+func main() {
+	user, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Hello %s! This is the Monkey programming language!\n",
+		user.Username)
+	fmt.Printf("Feel free to type in commands\n")
+	repl.Start(os.Stdin, os.Stdout)
 }
